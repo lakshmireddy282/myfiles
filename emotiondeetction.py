4a26c107c22bec798efb2b1177799c571871505e
lakshmireddy282 <85380215+lakshmireddy282@users.noreply.github.com>
2021-06-13 14:22:03 +0530

    Update emotiondeetction.py

--- emotiondeetction.py
+++ emotiondeetction.py
@@ -1,6 +1,6 @@
 import cv2  #pip install 
 from deepface import DeepFace #pip install deepface
-import matplotlib.pyplot as plt #pip install matplotlib
+
 im=cv2.imread('happy.jpg') #input the image 
 predictions=DeepFace.analyze(img) #using deepface library analyze the image
 print("The emotion in the image is :"+ predictions['dominant_emotion']) #out the emotion detected in the image using the deepface library.
